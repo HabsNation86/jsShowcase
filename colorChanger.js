12bab2bdf222f60361eff53b9ea80a12bae0cb6d
Jamie <31568475+HabsNation86@users.noreply.github.com>
2018-08-23 12:03:12 -0400

    Update colorChanger.js

--- colorChanger.js
+++ colorChanger.js
@@ -2,7 +2,7 @@ let clicker = document.querySelector("button");
 let isPurple = false;
 
 clicker.addEventListener("click", function (){
-// if white make purple
+
 if (isPurple){
   document.body.style.background = "white";
   isPurple= false;
