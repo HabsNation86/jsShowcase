ad813277e06497f8053691bc897b856665035bd4
Jamie <jamiemckeown469@gmail.com>
2018-08-23 17:48:00 -0400

    isolated digit using span

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -1,7 +1,13 @@
 
 let p1Button = document.querySelector("#p1");
 let p2Button = document.querySelector("#p2");
+let p1Display = document.querySelector("#p1Display");
+let p1Score = 0;
+
+console.log(p1Display);
 
 p1Button.addEventListener("click", function(){
-  console.log("Clicked");
+  p1Score++;
+  console.log(p1Score);
+  p1Display.textContent = p1Score;
 });
