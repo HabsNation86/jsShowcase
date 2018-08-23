d96302eb95858ec13b180f037aec261b0351fffe
Jamie <jamiemckeown469@gmail.com>
2018-08-23 13:31:33 -0400

    w/ refactoring

--- colorChanger.js
+++ colorChanger.js
@@ -1,15 +1,22 @@
 let clicker = document.querySelector("button");
 let isPurple = false;
 
-clicker.addEventListener("click", function (){
-// if white make purple
-if (isPurple){
-  document.body.style.background = "white";
-  isPurple= false;
-}
-else {
+// clicker.addEventListener("click", function (){
+// // if white make purple
+// if (isPurple){
+//   document.body.style.background = "white";
+//   isPurple= false;
+// }
+// else {
 
-  document.body.style.background = "purple";
-  isPurple = true;
-  }
-} );
+//   document.body.style.background = "purple";
+//   isPurple = true;
+//   }
+// } );
+
+
+// This is refactored version of above code
+
+clicker.addEventListener("click", function(){
+  document.body.classList.toggle("purple");
+});
