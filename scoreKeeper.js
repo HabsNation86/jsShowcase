8616260581c2344056d3c4f64d31e0c65003b19a
Jamie <jamiemckeown469@gmail.com>
2018-08-24 23:36:43 -0400

    reset and total

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -1,13 +1,30 @@
 
 let p1Button = document.querySelector("#p1");
 let p2Button = document.querySelector("#p2");
+let input = document.querySelector("#playToScore")
 let p1Display = document.querySelector("#p1Display");
+let p2Display = document.querySelector("#p2Display");
+let resetButton = document.querySelector("#reset");
 let p1Score = 0;
+let p2Score = 0;
 
-console.log(p1Display);
-
+// Player One Button
 p1Button.addEventListener("click", function(){
   p1Score++;
-  console.log(p1Score);
   p1Display.textContent = p1Score;
 });
+// Player Two Button
+p2Button.addEventListener("click", function(){
+  p2Score++;
+  p2Display.textContent = p2Score;
+});
+// Reset Button
+resetButton.addEventListener("click", function(){
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+});
+
+// Play to total
+playToScore.addEventListener("click", function(){
+  playToScore.textContent = input
+})
